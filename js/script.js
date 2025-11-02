8c6fcb65879972090c1315f3e77c4a2b64cd2277
João Guilherme <joaoguilhermediasp@gmail.com>
2025-11-02 15:03:03 -0300

    Enviando todos os arquivos

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+function enviarWhats(event) {
+    event.preventDefault()
+
+    const nome = document.getElementById('nome').value        
+    const mensagem = document.getElementById('mensagem').value
+    const telefone = '5514982305613'
+
+    const texto = `Olá! Me chamo ${nome}, ${mensagem}`
+    const msgFormatada = encodeURIComponent(texto)
+    const url = `https://whatsa.me/5514982305613/?t=${msgFormatada}`
+
+    window.open(url, '_blank')
+}
